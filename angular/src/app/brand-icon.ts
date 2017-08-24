e877b9b2acbb207071ad1e13620ca84e6a3160cd
Mike Shoemake <atlantatechie@gmail.com>
2017-08-23 22:10:55 -0400

    Removed Angular UI components.  This repo is now server-side only.

--- angular/src/app/brand-icon.ts
+++
@@ -1,9 +0,0 @@
-export class BrandIcon {
-	image: string = '';
-  displayText: string = '';
-
-	constructor(values: Object = {}) {
-		Object.assign(this, values);
-	}
-}
-
--- angular/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  logger: {
-    level: 5  // Level.LOG
-  }
-};
